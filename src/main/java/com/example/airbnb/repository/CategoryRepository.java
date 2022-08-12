fa172ff4714953cef1744ba7e0e29aa794859d79
vulinh1704 <truongdangvulinh1704@gmail.com>
2022-08-12 21:43:40 +0700

    git lần cuối - 12/08 - linh

--- src/main/java/com/example/airbnb/repository/CategoryRepository.java
+++ src/main/java/com/example/airbnb/repository/CategoryRepository.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     @Modifying
-    @Query(value = "select * from category where user_id = :id", nativeQuery = true)
+    @Query(value = "select * from category where user_id = :id order by id DESC", nativeQuery = true)
     Iterable<Category> findAllByUserId(@PathVariable Long id);
 
     @Modifying
-    @Query(value = "select * from category where status = :num and user_id = :id", nativeQuery = true)
+    @Query(value = "select * from category where status = :num and user_id = :id order by id DESC", nativeQuery = true)
     Iterable<Category> findAllByStatus(@PathVariable int num, @PathVariable Long id);
 }
